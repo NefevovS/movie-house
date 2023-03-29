8a73bfe2d661e95eec8f408eb819a3552e0075bc
Nefedov Sergey <nefedov38@gmail.com>
2023-03-29 17:35:00 +0300

    development plan

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import "./App.css";
 import { FilmServise } from "./API/FilmServise";
+import { useEffect, useState } from "react";
 function App() {
+  const [films, setFilms] = useState([]);
   const fetchFilm = async () => {
     const response = await FilmServise.getAll();
-    console.log(response.data);
+    setFilms(response.data.data.movies);
+    console.log(response.data.data.movies);
   };
+  useEffect(() => {
+    fetchFilm();
+  }, []);
 
-  fetchFilm();
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div style={{ display: "flex", flexWrap: "wrap" }}>
+        {films.map((film) => (
+          <div style={{ width: "400px" }}>
+            <div>{film.title}</div>
+            <img src={film.medium_cover_image} />
+            <div>{film.synopsis}</div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Components/FilmCard/FilmCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const FilmCard = () => {
+  return (
+    <div>
+      картинка название год выпуска оверлей>>>> звездочка рейтинг жанры кнопка с
+      фильмом
+    </div>
+  );
+};
+
+export default FilmCard;
