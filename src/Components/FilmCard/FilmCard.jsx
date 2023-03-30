0eeccdae73e49344862b2582aa879fafd5a79556
Nefedov Sergey <nefedov38@gmail.com>
2023-03-30 17:02:28 +0300

    add overlay logic

--- src/Components/FilmCard/FilmCard.jsx
+++ src/Components/FilmCard/FilmCard.jsx
@@ -2,11 +2,22 @@ import React, { useState } from "react";
 import s from "./FilmCard.module.css";
 
 const FilmCard = ({ imgSrc, movieTitle, movieYear }) => {
+  const [mouseOntheCard, setMouseOnTheCard] = useState(false);
   return (
     <div className={s.movieWrapper}>
-      <div className={s.movieCover__wrapper}>
+      <div
+        className={s.movieCover__wrapper}
+        onMouseEnter={() => setMouseOnTheCard(true)}
+        onMouseLeave={() => setMouseOnTheCard(false)}
+      >
         <img src={imgSrc} alt="image" className={s.movieCover} />
-        <div className={s.overlay}>
+        <div
+          className={
+            mouseOntheCard
+              ? [s.overlay, s.active].join(" ")
+              : [s.overlay].join(" ")
+          }
+        >
           <div>звездочка</div>
           <div>рейтинг</div>
           <div>жанры</div>
