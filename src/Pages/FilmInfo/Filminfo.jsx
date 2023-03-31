5c33a0df9453cc8b9638a8d8ae12dbbaea5b999e
Nefedov Sergey <nefedov38@gmail.com>
2023-03-31 15:05:40 +0300

    add custom hook and scss Loader

--- src/Pages/FilmInfo/Filminfo.jsx
+++ src/Pages/FilmInfo/Filminfo.jsx
@@ -1,22 +1,25 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { FilmServise } from "../../API/FilmServise";
+import Loader from "../../Components/Loader/Loader";
+import { useFetching } from "../../hooks/useFetching";
 
 const Filminfo = () => {
   const params = useParams();
-  const [film, setFilm] = useState();
+  const [film, setFilm] = useState([]);
+
   const fetchFilm = async () => {
     const response = await FilmServise.getFilmByID(params.id);
     setFilm(response.data.data.movie);
-    console.log(response.data.data.movie);
   };
+  const [fetching, isLoading, error] = useFetching(fetchFilm);
   useEffect(() => {
-    fetchFilm();
+    fetching();
   }, []);
 
   return (
     <div style={{ paddingTop: "200px", color: "white" }}>
-      <span>{film.id}</span>
+      {isLoading ? <Loader /> : <div>{film.id}</div>}
     </div>
   );
 };
